db9b89b74bd81b5d3e33601b5eff82df83afaca2
mackeee-orange <makino.aaa@gmail.com>
2022-02-25 16:54:57 +0900

    fix: update params

--- src/components/model/task/TaskListItem/requests/updateTask.ts
+++ src/components/model/task/TaskListItem/requests/updateTask.ts
@@ -1,7 +1,7 @@
 import { ApiClient } from "dataAccess/api";
 import { TaskRepository } from "domain/repository";
 
-export const updateTask: TaskRepository["update"] = async (id, name) => {
-  const res = await ApiClient.patch<AwaitType<TaskRepository["update"]>>(`/tasks/${id}`, { name });
+export const updateTask: TaskRepository["update"] = async (id, params) => {
+  const res = await ApiClient.patch<AwaitType<TaskRepository["update"]>>(`/tasks/${id}`, params);
   return res.data;
 };
